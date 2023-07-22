1717d5113ab34c11a42e34f53d237470a082ea14
Rakesh Kashyap <Rakesh Kashyap756890@gmail.com>
2023-07-22 07:43:31 +0530

    Color changes

--- Login_Page/script.js
+++ Login_Page/script.js
@@ -4,24 +4,24 @@ function FormValidation(){
     var pwd=document.getElementById('password').value;
     if( un==""||pwd==""){
         if( un==""&&pwd==""){
-            document.getElementById('UserName').style.borderColor = "red";
-            document.getElementById('password').style.borderColor = "red";
+            document.getElementById('UserName').style.borderColor = '#d20202';
+            document.getElementById('password').style.borderColor = '#d20202';
             return false;
         }
         else if(un==""&&pwd!=""){
-            document.getElementById('UserName').style.borderColor = "red";
-            document.getElementById('password').style.borderColor = "green";
+            document.getElementById('UserName').style.borderColor = '#d20202';
+            document.getElementById('password').style.borderColor = '#02a702';
             return false;
         }
         else if(un!=""&&pwd==""){
-            document.getElementById('UserName').style.borderColor = "green";
-            document.getElementById('password').style.borderColor = "red";
+            document.getElementById('UserName').style.borderColor = '#02a702';
+            document.getElementById('password').style.borderColor = '#d20202';
             return false;
         }
         else
     {
-        document.getElementById('UserName').style.borderColor = "green";
-        document.getElementById('password').style.borderColor = "green";
+        document.getElementById('UserName').style.borderColor = '#02a702';
+        document.getElementById('password').style.borderColor = '#02a702';
     }
    
 }
@@ -31,11 +31,11 @@ function NameValidation(){
     debugger
     var un=document.getElementById('UserName').value;
     if( un==""){
-        document.getElementById('UserName').style.borderColor = "red";
+        document.getElementById('UserName').style.borderColor = '#d20202';
     }
     else
     {
-        document.getElementById('UserName').style.borderColor = "green";
+        document.getElementById('UserName').style.borderColor = '#02a702';
     }
 }
 
@@ -43,10 +43,10 @@ function PwdValidation(){
     debugger
     var pwd=document.getElementById('password').value;
     if( pwd==""){
-        document.getElementById('password').style.borderColor = "red";
+        document.getElementById('password').style.borderColor = '#d20202';
     }
     else
     {
-        document.getElementById('password').style.borderColor = "green";
+        document.getElementById('password').style.borderColor = '#02a702';
     }
 }
